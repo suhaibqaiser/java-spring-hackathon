d53598879046fc85e964049260cb9dcabdb87ce9
suhaib qaiser <suhaib.guru@gmail.com>
2016-11-25 13:08:31 -0400

    Java Hackathon additional changes
    Included Java Spring basic implementation

---
+++ hackathon-sample-1/src/main/java/com/dal/hackathon/controllers/AppConfig.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.dal.hackathon.controllers;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author suhaib
+ *
+ */
+@Configuration
+@ComponentScan
+public class AppConfig {
+
+}
---
+++ hackathon-sample-1/src/main/java/com/dal/hackathon/controllers/GeneralController.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.dal.hackathon.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author suhaib
+ *
+ */
+@Controller
+public class GeneralController {
+
+	String message = "Welcome to Spring MVC!";
+
+	@RequestMapping("/hello")
+	public ModelAndView showMessage(
+			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+		System.out.println("in controller");
+
+		ModelAndView mv = new ModelAndView("helloworld");
+		mv.addObject("message", message);
+		mv.addObject("name", name);
+		return mv;
+	}
+}
