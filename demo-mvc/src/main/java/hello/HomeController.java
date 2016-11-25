a4432e838b18f82599c044257c323766df6378ce
suhaib qaiser <suhaib.guru@gmail.com>
2016-11-25 10:45:28 -0400

    Spring Boot Demo Application
    This will be used as part of tutorial

---
+++ demo-mvc/src/main/java/hello/HomeController.java
@@ -0,0 +1,21 @@
+package hello;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+	
+	@Value("${application.name}") 
+	private String name;
+
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "index";
+    }
+
+}
